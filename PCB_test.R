55c41a46da425ffb30a92cb466f7671055aac60f
Pierre Beukes <pierre.marise@gmail.com>
2020-10-20 11:13:04 +1300

    Commit updates

--- PCB_test.R
+++ PCB_test.R
@@ -2,3 +2,6 @@
 
 library(usethis)
 use_git_config(user.name = "Pierre Beukes", user.email = "pierre.marise@gmail.com")
+usethis::use_github()
+usethis::browse_github_pat()
+usethis::edit_r_environ()
